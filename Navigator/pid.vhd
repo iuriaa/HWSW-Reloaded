e03946b297ca232204cb6d77edb3dbba5ddcd18c
Shehzad Ahmed <shehzi001@gmail.com>
2015-03-21 23:43:52 +0100

    tuned proportional gain of the pid controller
    applied right shift by 2 bits on input error of the pid controller to
    reduce proportional gain.

--- Navigator/pid.vhd
+++ Navigator/pid.vhd
@@ -16,18 +16,31 @@ architecture Behavioral of pid is
 
 begin
 	process(reset, e_in)
+	   variable error_temp: signed(12 downto 0) := (OTHERS => '0');
+		variable integral_temp: signed(12 downto 0) := (OTHERS => '0');
 		variable u1: signed(12 downto 0) := (OTHERS => '0');
 		variable u_prev : signed(12 downto 0) := (OTHERS => '0');
-		constant k_pro: signed(6 downto 0):="0000001";
-		constant k_integral: signed( 6 downto 0 ):="0000001";
+		constant k_pro: signed(6 downto 0):="0000010";
+		constant k_integral: signed( 6 downto 0 ):="0000010";
 		--constant k2:std_logic_vector( 6 downto 0):="1101000";
 		--constant k3: std_logic_vector( 6 downto 0) :="0000010";
 	begin
 	   if reset ='1' then
 			u_prev := (OTHERS => '0');
 		elsif (clk'event and clk = '1') then
-				u1 := u_prev + (k_pro*(e_in));-- + (k_integral*integral);
-				u_prev := u1;
+		   if(integral >= 0) then
+				integral_temp := (abs(integral) srl 2);
+			else
+				integral_temp := -1*(abs(integral) srl 2);
+			end if;
+
+			if(e_in >= 0) then
+				error_temp := (abs(e_in) srl 2);
+			else
+				error_temp := -1*(abs(e_in) srl 2);
+			end if;
+			u1 := u_prev + (k_pro*(error_temp)) + (k_integral*integral_temp);
+			u_prev := u1;
 		end if;
 		u_out <= u1;
 	end process;
